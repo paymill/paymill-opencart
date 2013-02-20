82648dc3f704fd29465c772beaf7f8b85b3a2541
Harenkamp <stefan.harenkamp@payintelligent.de>
2013-02-20 09:45:36 +0100

    PM-110 Language wird nicht korrekt geladen - fixed

--- catalog/language/english/payment/paymilldirectdebit.php
+++ catalog/language/english/payment/paymilldirectdebit.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))).'/paymill/language/en/paymill.php';
+require dirname(dirname(dirname(dirname(dirname(__FILE__))))).'/paymill/language/en/paymill.php';
 // Heading
 $_['heading_title'] = $_['text_payment_paymilldirectdebit_title'];
--- catalog/language/german/payment/paymillcreditcard.php
+++ catalog/language/german/payment/paymillcreditcard.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once dirname(dirname(dirname(dirname(dirname(__FILE__))))).'/paymill/language/de/paymill.php';
+require dirname(dirname(dirname(dirname(dirname(__FILE__))))).'/paymill/language/de/paymill.php';
 // Heading
 $_['heading_title'] = $_['text_payment_paymillcreditcard_title'];
